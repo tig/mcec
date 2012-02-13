c92aebb23d751fd733715229aa207cfd3460f6f0
Charlie Kindel <tig@users.noreply.github.com>
2012-02-12 19:53:30 -0800

    Changed license from BSD to MIT

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@
 // http://www.kindel.com
 // charlie@kindel.com
 // 
-// Published under the BSD License.
+// Published under the MIT License.
 // Source control on SourceForge 
 //    http://sourceforge.net/projects/mcecontroller/
 //-------------------------------------------------------------------
@@ -35,7 +35,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.4.0.*")]
+[assembly: AssemblyVersion("1.4.1.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
