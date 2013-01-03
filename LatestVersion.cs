4e68f416e3c1252e44acb65bd5dee9def112ad43
Charlie Kindel <tig@users.noreply.github.com>
2013-01-02 18:18:46 -0800

    Cleaned up

--- LatestVersion.cs
+++ LatestVersion.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
@@ -11,33 +9,23 @@ namespace MCEControl
     class LatestVersion
     {
         public delegate void GotVersionInfo(object sender, string version);
-        public LatestVersion() {
-        }
 
-        public static Version CurrentVersion
-        {
+        public static Version CurrentVersion{
             get { return new Version(Application.ProductVersion); }
-            set { }
         }
-
 
         public String ErrorMessage { get; set; }
         public String LatestStableRelease { get; set; }
 
         public async void GetLatestStableVersion(GotVersionInfo callback) {
-
             var client = new WebClient();
-
             try {
                 byte[] bytes =
-                    await
-                    client.DownloadDataTaskAsync(
+                    await client.DownloadDataTaskAsync(
                         "http://mcec.codeplex.com/wikipage?title=Latest%20Stable%20Version%20Number");
 
-                Stream s = new MemoryStream(bytes);
-
                 var htmlDoc = new HtmlDocument();
-                htmlDoc.Load(s);
+                htmlDoc.Load(new MemoryStream(bytes));
 
                 var div = htmlDoc.DocumentNode.SelectSingleNode("//*/div[@class='wikidoc']");
                 if (div != null)
@@ -48,22 +36,16 @@ namespace MCEControl
             }
             catch (Exception e) {
                 ErrorMessage = e.Message;
-                Debug.Write(e.Message);
             }
             callback(this, LatestStableRelease);
         }
 
-        /// <summary>
-        /// > 0 - Newer version available
-        /// = 0 - Same version
-        /// < 0 - Current version is newer.
-        /// </summary>
-        /// <returns></returns>
+        // > 0 - Newer version available
+        // = 0 - Same version
+        // < 0 - Current version is newer
         public int CompareVersions() {
             var latest = new Version(LatestStableRelease);
-
             return CurrentVersion.CompareTo(latest);
         }
-
     }
 }
